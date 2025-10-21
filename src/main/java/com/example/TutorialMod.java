3acfa2e59ed0c9d750943b03d49eb201e62eaf82
jerryhaha <jarenparks@icloud.com>
2025-10-20 22:09:42 -0500

    added raw and ingot form pink garnet

--- src/main/java/com/example/TutorialMod.java
+++ src/main/java/com/example/TutorialMod.java
@@ -1,16 +1,17 @@
 package com.example;
 
+import com.example.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorial_mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
