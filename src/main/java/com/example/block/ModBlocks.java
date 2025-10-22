38c4b3d2596f31dc4c12e2d8070d2cdeabec0f63
jerryhaha <jarenparks@icloud.com>
2025-10-21 19:42:02 -0500

    added garnet ore variants and custom chisel item

--- src/main/java/com/example/block/ModBlocks.java
+++ src/main/java/com/example/block/ModBlocks.java
@@ -4,6 +4,7 @@ import com.example.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,18 +12,29 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     //PINK GARNET BLOCK
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
+    //RAW PINK GARNET BLOCK
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(2f)
                     .requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
-
-
+    //PINK GARNET ORE BLOCK
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create()
+                            .requiresTool().strength(3f,3f)
+                            .sounds(BlockSoundGroup.STONE)));
+    //PINK GARNET DEEPSLATE ORE BLOCK
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create()
+                            .requiresTool().strength(4.5f,3f)
+                            .sounds(BlockSoundGroup.STONE)));
 
 
 
@@ -42,6 +54,7 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_BLOCK);
             fabricItemGroupEntries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+            fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_ORE);
         });
     }
 }
